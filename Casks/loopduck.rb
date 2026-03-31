84ba85d4beec05db80f7caa50db3b28350416fde
Pituk K <pituk.kaewwsuksai@gmail.com>
2026-03-31 22:51:05 +0700

    test: use local file URL

--- Casks/loopduck.rb
+++ Casks/loopduck.rb
@@ -2,7 +2,7 @@ cask "loopduck" do
   version "0.1.2"
   sha256 "6e3d4789a880ceb651e34d87a66d0256a871fb69f702a98506d98e48f9b2db05"
 
-  url "https://buildonclaw.cloud/products/loopduck/release/LoopDuck_#{version}_developer-preview.dmg"
+  url "file:///Users/lps/BemindLabs/build-on-openclaw/web-build-on-claw/public/products/loopduck/release/LoopDuck_0.1.2_developer-preview.dmg"
   name "LoopDuck"
   desc "AI Coding workflow desktop app"
   homepage "https://buildonclaw.cloud/products/loopduck"
