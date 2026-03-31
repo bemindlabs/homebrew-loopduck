8a0454385aebe317119a2b95b142f53aa3518071
Pituk K <pituk.kaewwsuksai@gmail.com>
2026-03-31 22:50:46 +0700

    Add LoopDuck cask v0.1.2

---
+++ Casks/loopduck.rb
@@ -0,0 +1,20 @@
+cask "loopduck" do
+  version "0.1.2"
+  sha256 "6e3d4789a880ceb651e34d87a66d0256a871fb69f702a98506d98e48f9b2db05"
+
+  url "https://buildonclaw.cloud/products/loopduck/release/LoopDuck_#{version}_developer-preview.dmg"
+  name "LoopDuck"
+  desc "AI Coding workflow desktop app"
+  homepage "https://buildonclaw.cloud/products/loopduck"
+
+  depends_on macos: ">= :ventura"
+
+  app "LoopDuck.app"
+
+  zap trash: [
+    "~/Library/Application Support/com.bemindlabs.loopduck",
+    "~/Library/Caches/com.bemindlabs.loopduck",
+    "~/Library/Preferences/com.bemindlabs.loopduck.plist",
+    "~/Library/Saved Application State/com.bemindlabs.loopduck.savedState",
+  ]
+end
