00f6b21f435e27a333396ee569e1e82b8771e784
Pituk K <pituk.kaewwsuksai@gmail.com>
2026-04-01 08:26:02 +0700

    chore: switch back to DMG distribution

--- Casks/loopduck.rb
+++ Casks/loopduck.rb
@@ -1,23 +1,21 @@
 cask "loopduck" do
   version "0.1.4"
-  sha256 "fe211d79e85feec7714955b9b45ac80bfaa2d802ad7a309501164580d8d7cd10"
+  sha256 "9ba47b26278f6f319a4ade8ff8c9a8c2223f1c434c972daf6d39ef23c74cf4da"
 
-  url "https://buildonclaw.cloud/products/loopduck/release/LoopDuck_#{version}_aarch64.pkg"
+  url "https://buildonclaw.cloud/products/loopduck/release/LoopDuck_#{version}_aarch64.dmg"
   name "LoopDuck"
   desc "AI Coding workflow desktop app"
   homepage "https://buildonclaw.cloud/products/loopduck"
 
   depends_on macos: ">= :ventura"
 
-  pkg "LoopDuck_#{version}_aarch64.pkg"
+  app "LoopDuck.app"
 
   postflight do
     system_command "/usr/bin/xattr",
                    args: ["-cr", "#{appdir}/LoopDuck.app"],
                    sudo: true
   end
-
-  uninstall pkgutil: "com.bemindlabs.loopduck"
 
   zap trash: [
     "~/Library/Application Support/com.bemindlabs.loopduck",
